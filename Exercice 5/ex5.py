f4199ed99bfcd36da17623e50845385c6d73746e
Stooms <touzet.stan@protonmail.com>
2022-11-26 13:45:55 +0100

    Architecture de l'exercice

---
+++ Exercice 5/ex5.py
@@ -0,0 +1,232 @@
+# Machine Learning Online Class
+#  Exercise 5 | Regularized Linear Regression and Bias-Variance
+#
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the
+#  exercise. You will need to complete the following functions:
+#
+#     linearRegCostFunction.m
+#     learningCurve.m
+#     validationCurve.m
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+
+# Initialization
+
+import numpy as np
+from scipy.io import loadmat
+import functions as f
+import matplotlib.pyplot as plt
+
+# =========== Part 1: Loading and Visualizing Data =============
+#  We start the exercise by first loading and visualizing the dataset. 
+#  The following code will load the dataset into your environment and plot
+#  the data.
+#
+
+# Load Training Data
+print('Loading and Visualizing Data ...\n')
+
+# Load from ex5data1: 
+# You will have X, y, Xval, yval, Xtest, ytest in your environment
+data = loadmat('../Data/ex5data1.mat')
+X = data['X']
+Xtest = data['Xtest']
+Xval = data['Xval']
+y = data['y'].reshape((np.size(data['y']),))
+ytest = data['ytest'].reshape((np.size(data['ytest']),))
+yval = data['yval'].reshape((np.size(data['yval']),))
+
+# m = Number of examples
+m = np.size(X, 0)
+
+# Plot training data
+plt.plot(X, y, 'rx', ms=10, lw=1.5)
+plt.xlabel('Change in water level (x)')
+plt.ylabel('Water flowing out of the dam (y)')
+plt.show()
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+# =========== Part 2: Regularized Linear Regression Cost =============
+#  You should now implement the cost function for regularized linear 
+#  regression. 
+#
+Xc = np.concatenate((np.ones((m, 1)), X), axis=1)
+theta = np.array([1, 1])
+J = f.linearRegCostFunction(Xc, y, theta, 1)
+
+print('Cost at theta = [1  1]: {:2f}'
+    '\n(this value should be about 303.993192)\n'.format(J))
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+# =========== Part 3: Regularized Linear Regression Gradient =============
+#  You should now implement the gradient for regularized linear 
+#  regression.
+#
+
+theta = np.array([1, 1])
+J = f.linearRegCostFunction(Xc, y, theta, 1)
+grad = f.linearRegGradCostFunction(Xc, y, theta, 1)
+
+print('Gradient at theta = [1  1]:  [{:2f} {:2f}] '
+    '\n(this value should be about [-15.303016 598.250744])\n'.format(grad[0], grad[1]))
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+
+# =========== Part 4: Train Linear Regression =============
+#  Once you have implemented the cost and gradient correctly, the
+#  trainLinearReg function will use your cost function to train 
+#  regularized linear regression.
+# 
+#  Write Up Note: The data is non-linear, so this will not give a great 
+#                 fit.
+#
+
+#  Train linear regression with lambda = 0
+lambda_t = 0
+theta = f.trainLinearReg(Xc, y, lambda_t)
+
+#  Plot fit over the data
+plt.plot(X, y, 'rx', ms=10, lw=1.5)
+plt.xlabel('Change in water level (x)')
+plt.ylabel('Water flowing out of the dam (y)')
+plt.plot(X, Xc @ theta, lw=2)
+plt.show()
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+
+# =========== Part 5: Learning Curve for Linear Regression =============
+#  Next, you should implement the learningCurve function. 
+#
+#  Write Up Note: Since the model is underfitting the data, we expect to
+#                 see a graph with "high bias" -- Figure 3 in ex5.pdf 
+#
+
+lambda_t = 0
+Xvalc = np.concatenate((np.ones((np.size(Xval, 0), 1)), Xval), axis=1)
+error_train, error_val = f.learningCurve(Xc, y, Xvalc, yval, lambda_t)
+
+plt.plot(np.arange(1, m+1), error_train, label='Train')
+plt.plot(np.arange(1, m+1), error_val, label='Cross Validation')
+plt.title('Learning curve for linear regression')
+plt.legend()
+plt.xlabel('Number of training examples')
+plt.ylabel('Error')
+plt.xlim([0, 13])
+plt.ylim([0, 150])
+plt.show()
+
+print('# Training Examples\tTrain Error\tCross Validation Error\n')
+# for i in range(m):
+#     print('\t{:d}\t\t{:2f}\t{:2f}\n'.format(i, error_train[i], error_val[i]))
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+# =========== Part 6: Feature Mapping for Polynomial Regression =============
+#  One solution to this is to use polynomial regression. You should now
+#  complete polyFeatures to map each example into its powers
+#
+
+p = 8
+
+# Map X onto Polynomial Features and Normalize
+X_poly = f.polyFeatures(X, p)
+X_poly, mu, sigma = f.featureNormalize(X_poly)  # Normalize
+X_poly = np.concatenate((np.ones((m, 1)), X_poly), axis=1)                  # Add Ones
+
+# Map X_poly_test and normalize (using mu and sigma)
+X_poly_test = f.polyFeatures(Xtest, p)
+X_poly_test -= mu
+X_poly_test /= sigma
+X_poly_test = np.concatenate((np.ones((np.size(X_poly_test, 0), 1)), X_poly_test), axis=1)      # Add Ones
+
+
+# Map X_poly_val and normalize (using mu and sigma)
+X_poly_val = f.polyFeatures(Xval, p)
+X_poly_val -= mu
+X_poly_val /= sigma
+X_poly_val = np.concatenate((np.ones((np.size(X_poly_val, 0), 1)), X_poly_val), axis=1)         # Add Ones
+
+print('Normalized Training Example 1:\n')
+# print('  {:2f}  \n'.format(X_poly[0, :]))
+
+print('\nProgram paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+
+# =========== Part 7: Learning Curve for Polynomial Regression =============
+#  Now, you will get to experiment with polynomial regression with multiple
+#  values of lambda. The code below runs polynomial regression with 
+#  lambda = 0. You should try running the code with different values of
+#  lambda to see how the fit and learning curve change.
+#
+
+lambda_t = 0
+theta = f.trainLinearReg(X_poly, y, lambda_t)
+
+# Plot training data and fit
+# figure(1)
+plt.plot(X, y, 'rx', ms=10, lw=1.5)
+f.plotFit(np.min(X), np.max(X), mu, sigma, theta, p)
+plt.xlabel('Change in water level (x)')
+plt.ylabel('Water flowing out of the dam (y)')
+plt.title('Polynomial Regression Fit (lambda = {:2f})'.format(lambda_t))
+plt.show()
+
+error_train, error_val = f.learningCurve(X_poly, y, X_poly_val, yval, lambda_t)
+plt.plot(np.arange(1, m + 1), error_train, label='Train')
+plt.plot(np.arange(1, m + 1), error_val, label='Cross Validation')
+
+plt.title('Polynomial Regression Learning Curve (lambda = {:2f})'.format(lambda_t))
+plt.xlabel('Number of training examples')
+plt.ylabel('Error')
+plt.xlim([0, 13])
+plt.ylim([0, 100])
+plt.legend()
+plt.show()
+
+print('Polynomial Regression (lambda = {:2f})\n\n'.format(lambda_t))
+print('# Training Examples\tTrain Error\tCross Validation Error\n')
+# for i in range(m):
+#     print('\t{:d}\t\t{:2f}\t{:2f}\n'.format(i, error_train[i], error_val[i]))
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
+
+# =========== Part 8: Validation for Selecting Lambda =============
+#  You will now implement validationCurve to test various values of 
+#  lambda on a validation set. You will then use this to select the
+#  "best" lambda value.
+#
+
+lambda_vec, error_train, error_val = f.validationCurve(X_poly, y, X_poly_val, yval)
+
+
+plt.plot(lambda_vec, error_train, label='Train')
+plt.plot(lambda_vec, error_val, label='Cross Validation')
+plt.legend()
+plt.xlabel('lambda')
+plt.ylabel('Error')
+plt.show()
+
+print('lambda\t\tTrain Error\tValidation Error\n')
+
+# for i in range(len(lambda_vec)):
+#     print(' {:2f}\t{:2f}\t{:2f}\n'.format(lambda_vec[i], error_train[i], error_val[i]))
+
+
+print('Program paused. Press enter to continue.\n')
+input('Press enter to continue')
---
+++ Exercice 5/functions.py
@@ -0,0 +1,231 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin_cg
+
+
+def featureNormalize(X):
+    # FEATURENORMALIZE Normalizes the features in X
+    #   FEATURENORMALIZE(X) returns a normalized version of X where
+    #   the mean value of each feature is 0 and the standard deviation
+    #   is 1. This is often a good preprocessing step to do when
+    #   working with learning algorithms.
+    
+    mu = np.mean(X)
+    X_norm = X - mu
+    
+    sigma = np.std(X_norm)
+    X_norm /= sigma
+
+    return X_norm, mu, sigma
+
+
+def learningCurve(X, y, Xval, yval, lambda_t):
+    # LEARNINGCURVE Generates the train and cross validation set errors needed
+    # to plot a learning curve
+    #   [error_train, error_val] = ...
+    #       LEARNINGCURVE(X, y, Xval, yval, lambda) returns the train and
+    #       cross validation set errors for a learning curve. In particular,
+    #       it returns two vectors of the same length - error_train and
+    #       error_val. Then, error_train(i) contains the training error for
+    #       i examples (and similarly for error_val(i)).
+    #
+    #   In this function, you will compute the train and test errors for
+    #   dataset sizes from 1 up to m. In practice, when working with larger
+    #   datasets, you might want to do this in larger intervals.
+    #
+
+# Number of training examples
+    m = np.size(X, 0)
+
+    # You need to return these values correctly
+    error_train = np.zeros((m, 1))
+    error_val = np.zeros((m, 1))
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Fill in this function to return training errors in
+    #               error_train and the cross validation errors in error_val.
+    #               i.e., error_train(i) and
+    #               error_val(i) should give you the errors
+    #               obtained after training on i examples.
+    #
+    # Note: You should evaluate the training error on the first i training
+    #       examples (i.e., X(1:i, :) and y(1:i)).
+    #
+    #       For the cross-validation error, you should instead evaluate on
+    #       the _entire_ cross validation set (Xval and yval).
+    #
+    # Note: If you are using your cost function (linearRegCostFunction)
+    #       to compute the training and cross validation error, you should
+    #       call the function with the lambda argument set to 0.
+    #       Do note that you will still need to use lambda when running
+    #       the training to obtain the theta parameters.
+    #
+    # Hint: You can loop over the examples with the following:
+    #
+    #       for i = 1:m
+    #           # Compute train/cross validation errors using training examples
+    #           # X(1:i, :) and y(1:i), storing the result in
+    #           # error_train(i) and error_val(i)
+    #           ....
+    #
+    #       end
+    #
+
+    # ---------------------- Sample Solution ----------------------
+
+
+
+
+
+
+
+    # -------------------------------------------------------------
+
+    # =========================================================================
+
+    return error_train, error_val
+
+
+def linearRegCostFunction(X, y, theta, lambda_t):
+    # LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
+    # regression with multiple variables
+    #   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+    #   cost of using theta as the parameter for linear regression to fit the
+    #   data points in X and y. Returns the cost in J and the gradient in grad
+
+    # Initialize some useful values
+    m = len(y)  # number of training examples
+
+    # You need to return the following variables correctly
+    J = 0
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Compute the cost and gradient of regularized linear
+    #               regression for a particular choice of theta.
+    #
+    #               You should set J to the cost and grad to the gradient.
+    #
+    # =========================================================================
+
+    return J
+
+
+def linearRegGradCostFunction(X, y, theta, lambda_t):
+
+    m = len(y)
+    grad = np.zeros(theta.shape)
+
+    grad = grad.T.ravel()
+    return grad
+
+
+def plotFit(min_x, max_x, mu, sigma, theta, p):
+    # PLOTFIT Plots a learned polynomial regression fit over an existing figure.
+    # Also works with linear regression.
+    #   PLOTFIT(min_x, max_x, mu, sigma, theta, p) plots the learned polynomial
+    #   fit with power p and feature normalization (mu, sigma).
+
+    # Hold on to the current figure
+
+    # We plot a range slightly bigger than the min and max values to get
+    # an idea of how the fit will vary outside the range of the data points
+    x = (min_x - np.arange(15, max_x + 25, 0.05).T)
+
+    # Map the X values
+    X_poly = polyFeatures(x, p)
+    X_poly -= mu
+    X_poly /= sigma
+
+    # Add ones
+    X_poly = np.concatenate((np.ones((np.size(x, 0), 1)), X_poly), axis=1)
+
+    # Plot
+    plt.plot(x, X_poly @ theta, lw=2)
+
+
+def polyFeatures(X, p):
+    # POLYFEATURES Maps X (1D vector) into the p-th power
+    #   [X_poly] = POLYFEATURES(X, p) takes a data matrix X (size m x 1) and
+    #   maps each example into its polynomial features where
+    #   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
+
+    # You need to return the following variables correctly.
+    X_poly = np.zeros((np.size(X), p))
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Given a vector X, return a matrix X_poly where the p-th
+    #               column of X contains the values of X to the p-th power.
+    #
+    #
+    # =========================================================================
+
+    return X_poly
+
+
+def trainLinearReg(X, y, lambda_t):
+    # TRAINLINEARREG Trains linear regression given a dataset (X, y) and a
+    # regularization parameter lambda
+    #   [theta] = TRAINLINEARREG (X, y, lambda) trains linear regression using
+    #   the dataset (X, y) and regularization parameter lambda. Returns the
+    #   trained parameters theta.
+    #
+
+    # Initialize Theta
+
+    initial_theta = np.zeros((np.size(X, 1), 1))
+
+    # Create "short hand" for the cost function to be minimized
+    costFunction = lambda t: linearRegCostFunction(X, y, t, lambda_t)
+    gradCostFunction = lambda t: linearRegGradCostFunction(X, y, t, lambda_t)
+
+    output = fmin_cg(costFunction, initial_theta, gradCostFunction,
+            maxiter=200,
+            full_output=True)
+    theta = output[0]
+
+    return theta
+
+
+def validationCurve(X, y, Xval, yval):
+    # VALIDATIONCURVE Generate the train and validation errors needed to
+    # plot a validation curve that we can use to select lambda
+    #   [lambda_vec, error_train, error_val] = ...
+    #       VALIDATIONCURVE(X, y, Xval, yval) returns the train
+    #       and validation errors (in error_train, error_val)
+    #       for different values of lambda. You are given the training set (X,
+    #       y) and validation set (Xval, yval).
+    #
+
+    # Selected values of lambda (you should not change this)
+    lambda_vec = np.array([0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]).T
+
+    # You need to return these variables correctly.
+    error_train = np.zeros((len(lambda_vec), 1))
+    error_val = np.zeros((len(lambda_vec), 1))
+
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: Fill in this function to return training errors in
+    #               error_train and the validation errors in error_val. The
+    #               vector lambda_vec contains the different lambda parameters
+    #               to use for each calculation of the errors, i.e,
+    #               error_train(i), and error_val(i) should give
+    #               you the errors obtained after training with
+    #               lambda = lambda_vec(i)
+    #
+    # Note: You can loop over lambda_vec with the following:
+    #
+    #       for i = 1:length(lambda_vec)
+    #           lambda = lambda_vec(i);
+    #           # Compute train / val errors when training linear
+    #           # regression with regularization parameter lambda
+    #           # You should store the result in error_train(i)
+    #           # and error_val(i)
+    #           ....
+    #
+    #       end
+    #
+    #
+    # =========================================================================
+
+    return lambda_vec, error_train, error_val
+    
